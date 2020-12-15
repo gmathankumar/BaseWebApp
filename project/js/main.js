c4e383b15be135341341b196b97da9b8e66702dd
Mathan Kumar <gmathankumar.24@gmail.com>
2020-12-15 23:34:58 +0000

    Adding weather API

--- project/js/main.js
+++ project/js/main.js
@@ -1,4 +1,16 @@
+$(document).ready(function(){
+  getWeather();
+})
 
+function getWeather(){
+var url = "https://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid="+apikey;
+
+$.ajax(url,{success: function(data){
+  $(".city").text(data.name);
+  $(".temp").text(data.main.temp);
+}})
+
+}
 function myFunction() {
   // use jQuery ($ is shorthand) to find the div on the page and then change the html
   // jQuery can do a lot of crazy stuff so make sure to google around to find out more
